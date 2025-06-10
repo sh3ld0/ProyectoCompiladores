c37ce5fa43926963a758a40c5f613d6c83ffd3dc
All23tor <acc.dummyagta1@gmail.com>
2025-06-10 12:35:15 -0500

    Added note length

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <map>
-#include <utility>
-#include <vector>
+#include <numeric>
+#include <sstream>
 
 namespace Music {
 enum class Tone {
@@ -21,25 +21,61 @@ enum class Tone {
   B = 11
 };
 
+struct NoteLength {
+  int num, dem;
+
+  int to_int() const {
+    return num / dem;
+  }
+
+  NoteLength operator*(int n) const {
+    int f = std::gcd(num * n, dem);
+    return {num * n / f, dem / f};
+  }
+
+  friend std::ostream& operator<<(std::ostream& os,
+                                  const NoteLength& duration) {
+    return os << duration.num << '/' << duration.dem;
+  }
+
+  friend std::istream& operator>>(std::istream& is, NoteLength& duration) {
+    is >> duration.num;
+
+    if (is.peek() != '/') {
+      duration.dem = 1;
+    } else {
+      is.ignore(1);
+      is >> duration.dem;
+    }
+
+    return is;
+  }
+};
+
 const std::map<std::string, Tone> string_to_tone = {
     {"C", Tone::C},   {"C#", Tone::CS}, {"D", Tone::D},   {"D#", Tone::DS},
     {"E", Tone::E},   {"F", Tone::F},   {"F#", Tone::FS}, {"G", Tone::G},
     {"G#", Tone::GS}, {"A", Tone::A},   {"A#", Tone::AS}, {"B", Tone::B}};
 
-int note_to_pitch(std::string note) {
+std::pair<int, NoteLength> to_key_and_length(std::string&& note_str) {
+  std::stringstream ss(std::move(note_str));
+
   std::string note_tone = "";
   std::string note_octave = "";
 
-  for (char c : note)
+  for (char c = ss.get(); c != '-'; c = ss.get())
     if (std::isdigit(c))
       note_octave += c;
     else
       note_tone += c;
 
+  NoteLength length;
+  ss >> length;
+
   auto octave = 12 * std::stoi(note_octave);
   auto tone = std::to_underlying(string_to_tone.at(note_tone));
 
-  return tone + octave;
+  return {tone + octave, length};
 }
 }; // namespace Music
 
@@ -50,51 +86,34 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  std::ifstream archivo(argv[1]);
-  if (!archivo.is_open()) {
+  std::ifstream in_file(argv[1]);
+  if (!in_file.is_open()) {
     std::cerr << "No se pudo abrir el archivo " << argv[1] << '\n';
     return 1;
   }
 
-  std::vector<int> notas;
-  std::string palabra;
-  bool playMode = false;
-  // almacena las notas en un tipo de dato que acepta midi, en este caso numeros
-  // de 0 a 127
-  while (archivo >> palabra) {
-    if (palabra == "play") {
-      playMode = true;
-    } else if (playMode) {
-      int nota = Music::note_to_pitch(palabra);
-      if (nota != -1)
-        notas.push_back(nota);
-      else {
-        std::cout << "Símbolo no reconocido: " << palabra << '\n';
-        return 2;
-      }
-    }
-  }
-
-  archivo.close();
-
   smf::MidiFile midi;
-
-  int track = 0;
+  int measure_duration = 4 * 60;
+  midi.setTicksPerQuarterNote(measure_duration / 4);
   int tick = 0;
-  int duracion = 120; // duración de nota
+  int track = 0;
   int canal = 0;
-  int cont = 0;
-  for (auto nota : notas) {
-    cont++;
-    if (cont == 8) {
-      tick += duracion;
-    } else if (cont == 12) {
-      tick += duracion;
+
+  std::string word;
+  while (in_file >> word) {
+    auto [key, length] = Music::to_key_and_length(std::move(word));
+    if (key == -1) {
+      std::cout << "Símbolo no reconocido: " << word << '\n';
+      return 2;
     }
-    midi.addNoteOn(track, tick, canal, nota, 100);
-    midi.addNoteOff(track, tick + duracion, canal, nota);
-    tick += duracion;
+
+    int duration = (length * measure_duration).to_int();
+    midi.addNoteOn(track, tick, canal, key, 100);
+    midi.addNoteOff(track, tick + duration, canal, key);
+    tick += duration;
   }
+
+  in_file.close();
 
   midi.sortTracks(); // ordenar eventos por tiempo por si las dudas
   midi.write("salida.mid");
