d6a389548cc399cf6b242e02d366459864200ad8
All23tor <acc.dummyagta1@gmail.com>
2025-07-22 11:48:45 -0500

    Added some debug info for bars

--- src/Checker.cpp
+++ src/Checker.cpp
@@ -1,5 +1,6 @@
 #include "Checker.hpp"
 #include "Parser.hpp"
+#include <format>
 #include <print>
 
 namespace Checker {
@@ -18,17 +19,19 @@ void analyze(const Parser::Ast& ast, Music::Length signature) {
   Music::Length measure{0, 1};
   auto lengthed_node = bar_node->bar.get();
   while (lengthed_node) {
-    auto node_not = dynamic_cast<Parser::NoteNode*>(lengthed_node);
-    if (node_not) {
-      measure += node_not->note.length;
+    auto note_node = dynamic_cast<Parser::NoteNode*>(lengthed_node);
+    if (note_node) {
+      measure += note_node->note.length;
+      std::print("{}, ", note_node->note.length);
     } else {
       auto rest_node = dynamic_cast<Parser::RestNode*>(lengthed_node);
       measure += rest_node->rest.length;
+      std::print("{}, ", rest_node->rest.length);
     }
     lengthed_node = lengthed_node->next.get();
   }
 
-  std::print("Bar length: {}\n", measure);
+  std::println("Length {}", measure);
   if (measure != signature)
     throw Checker::LengthError(measure, signature);
 
