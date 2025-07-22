c048e1bc18599a1d5c223c29f3b5c85b6142cd30
All23tor <acc.dummyagta1@gmail.com>
2025-07-21 19:50:30 -0500

    Improved printing

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include "Checker.hpp"
 #include "Lexer.hpp"
 #include "Parser.hpp"
-#include <iostream>
+
+template <class... Args>
+void println_err(std::format_string<Args...> fmt, Args&&... args) {
+  std::println(std::cerr, fmt, std::forward<Args>(args)...);
+}
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
-    std::cerr << "Pass input file name as first argument\n"
-              << "and output file name as second argument\n";
+    println_err("Pass input file name as first argument");
+    println_err("and output file name as second argument");
     return 1;
   }
 
   std::ifstream file(argv[1]);
   if (!file.is_open()) {
-    std::cerr << "No se pudo abrir el archivo: " << argv[1] << '\n';
+    println_err("Could not open file: {}", argv[1]);
     return 1;
   }
 
   Lexer::Tokens tokens;
   try {
     tokens = Lexer::analyze(file);
-  } catch (std::invalid_argument& i_a) {
-    std::cerr << "Lexical error\n";
-    std::cerr << i_a.what() << '\n';
+  } catch (const std::invalid_argument& i_a) {
+    println_err("Lexical error: {}", i_a.what());
     return 1;
   }
 
@@ -29,17 +32,15 @@ int main(int argc, char* argv[]) {
   try {
     ast = Parser::analyze(tokens);
     ast->print(std::cout);
-  } catch (std::runtime_error& i_a) {
-    std::cerr << "Syntax error\n";
-    std::cerr << i_a.what() << '\n';
+  } catch (std::runtime_error& r_e) {
+    println_err("Syntax error: {}", r_e.what());
     return 1;
   }
 
   try {
     Checker::analyze(ast);
   } catch (Checker::SemanticError& s_e) {
-    std::cerr << "Semantics error\n";
-    std::cerr << s_e.what() << '\n';
+    println_err("Semantics error: {}", s_e.what());
     return 1;
   }
 
